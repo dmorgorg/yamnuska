46e7456a48ecf4229a419c4ece7bd55d4f950d97
dave morgan <dave@dmorg.org>
2024-12-12 13:22:50 +0000

    removing more images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 	// import MyImage from '$lib/imageDump/2008-07-01-170627.jpg?enhanced';
 
-	const imageModules = import.meta.glob('$lib/imageDump/*.jpg', {
+	const imageModules = import.meta.glob('$lib/imageDir /*.jpg', {
 		eager: true,
 		query: {
 			enhanced: true
@@ -11,10 +11,7 @@
 	});
 </script>
 
-<h1>Images galore?</h1>
-
 <div class="container">
-	<!-- <enhanced:img src={MyImage} alt="some alt text" class="enhanced" /> -->
 	{#each Object.entries(imageModules) as [_path, module]}
 		<enhanced:img src={module.default} alt="some alt text" class="enhanced" />
 	{/each}
